e877591c1cbd337aaa3e662d50ff86f4d815feb1
James O. D. Hunt <james.o.hunt@intel.com>
2017-01-05 11:49:19 +0000

    tests: Show output collected by expect on regex failure.
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- tests/rkt_tests.go
+++ tests/rkt_tests.go
@@ -369,14 +369,17 @@ func runRktAsUidGidAndCheckOutput(t *testing.T, rktCmd, expectedLine string, lin
 
 	if expectedLine != "" {
 		if lineIsRegex == true {
-			_, _, err = expectRegexWithOutput(child, expectedLine)
+			output, _, err := expectRegexWithOutput(child, expectedLine)
+			if err != nil {
+				t.Fatalf("didn't receive expected regex %q in output %v: %v", expectedLine, output, err)
+			}
 		} else {
 			err = expectWithOutput(child, expectedLine)
+			if err != nil {
+				t.Fatalf("didn't receive expected output %q: %v", expectedLine, err)
+			}
 		}
 
-		if err != nil {
-			t.Fatalf("didn't receive expected output %q (regex=%v): %v", expectedLine, lineIsRegex, err)
-		}
 	}
 }
 
